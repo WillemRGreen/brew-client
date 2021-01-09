34547a282bd74e73122457762d8d6b0b5e021adf
Willem Green <willberforce15@gmail.com>
2021-01-09 14:43:25 -0600

    added allcomponent folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <h1> This is the app </h1>
-  );
+class App extends Component {
+  state = {
+    brews: []
+  }
+
+  componentDidMount() {
+    if(TokenService.hasAuthToken()){
+      ApiService.getBrews()
+        .then((brews) => {
+          this.setState({ brews })
+        })
+        .catch(error => {
+          console.error({ error })
+        })
+    }
+  }
+
+  handleAddBrew = brew => {
+    this.setState({
+      brews: [
+        ...this.state.brews,
+        brew
+      ]
+    })
+  }
+
+  handleDeleteBrew = brewId => {
+    this.setState({
+      brews: this.state.brews.filter(brew => brew.id !== brewId)
+    })
+  }
+
+  handleLoggedIn = e => {
+      ApiService.getBrews()
+        .then((brews) => {
+          this.setState({ brews })
+        })
+        .catch(error => {
+          console.error({ error })
+        })
+  }
+
+  renderRoutes() {
+    
+  }
+
+  render() {
+    return (
+      <h1> This is the app </h1>
+    );
+  }
+  
 }
 
 export default App;
